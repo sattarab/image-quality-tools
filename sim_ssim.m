58f3bd85fa4d6273cdecd0ae80ab3f0d605ee344
Abdullah <abdullah.sattar@utoronto.ca>
2014-02-02 16:05:22 -0800

    updated ssim and psnr

--- sim_ssim.m
+++ sim_ssim.m
@@ -1,27 +1,21 @@
 function [SSIM_Value] = sim_ssim()
     % go to image directory not the best way but works %
     currDir = cd ('images');
-    [imageName] = textread('imageNames.data', '%s');
+    [threshold] = textread('threshold.data', '%s');
     n = 100;
     SSIM_Value = zeros(n, 5);
-    
-    for i = 1:length(imageName)
+    images = 3; % global variable for the image set 
+    for i = 1:images
         src = strcat('image',int2str(i),'*Orig.jpg');
-        copy = strcat('image',int2str(i),'*Copy.jpg');
-        fileNames = dir(copy);
         origFile = dir(src);
-        origFileName = imread(origFile(1).name);
-        C = cell(length(fileNames), 1);
-        for k = 1:length(fileNames)
-            filename = fileNames(k).name;
-            C{k} = imread(filename);
-        end
-        
-        for k = 1:length(fileNames)
-            SSIM_Value(i, k) = ssim(origFileName, C{k});
+        for j =  1:length(threshold)
+            copy = strcat('image',int2str(i),'*', threshold{j}, 'Copy.jpg');
+            fileNames = dir(copy);
+            origFileName = imread(origFile(1).name);
+            filename = fileNames.name;
+            SSIM_Value(i, j) = ssim(origFileName, imread(filename));
         end
     end
-    
     SSIM_Value = SSIM_Value(1:i, :);
     % go to src directory %
     cd ..;
