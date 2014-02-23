d0105a9f16f9db03298db5cc8e363decf64c7b97
sattarab <abdullah.sattar@utoronto.ca>
2014-02-22 21:54:18 -0800

    added script for changing the name of the images to convention

---
+++ changeName.m
@@ -0,0 +1,24 @@
+%script for reading the file names and converting it to the appropraite
+%name 
+
+%Directory of the files
+function changeName(location)
+    % example location = 'C:\Users\abdullah\Desktop\design project\DesignProject\Image Set\';
+    % Retrieve the name of the files only
+    names = dir(location);
+    names = {names(~[names.isdir]).name};
+    % Calculate the length of each name and the max length
+    len  = cellfun('length',names);
+    mLen = max(len); 
+    % Exclude from renaming the files long as the max 
+    idx   = len < mLen;
+    len   = len(idx);
+    names = names(idx);
+
+    % Rename in a LOOP
+    for n = 1:numel(names)
+        oldname = [location names{n}];
+        newname = strcat('images', int2str(n), '.jpg');
+        dos(['rename "' oldname '" "' newname '"']); % (1)
+    end
+end
